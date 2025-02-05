768c06322a12013439780dc2d12d22da33f48c83
MorganyJayaprakash <127740096+MorganyJayaprakash@users.noreply.github.com>
2025-02-05 09:57:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u morganyy -p 1234567890
+    docker login -u morganyy -p morgany2004
     docker tag test morganyy/task_2
     docker push morgany/task_2
     
