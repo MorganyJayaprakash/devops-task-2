cdf841f91223f23ba180f1aa4888e29b383653d4
MorganyJayaprakash <127740096+MorganyJayaprakash@users.noreply.github.com>
2025-02-05 10:19:20 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u morganyy -p morgany2004
-    docker tag test morganyy/task_2
-    docker push morgany/task_2
-    
